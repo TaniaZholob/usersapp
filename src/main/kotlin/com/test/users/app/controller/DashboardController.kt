8ada131ade113545a548839997c47eb7417404f8
TetianaZholob <tania.zholob@smartrockstars.com>
2026-05-29 15:50:29 +0300

    change controller for pagination

--- src/main/kotlin/com/test/users/app/controller/DashboardController.kt
+++ src/main/kotlin/com/test/users/app/controller/DashboardController.kt
@@ -2,9 +2,9 @@ package com.test.users.app.controller
 
 import com.test.users.app.dto.UserDto
 import com.test.users.app.service.UserService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/users")
@@ -13,7 +13,10 @@ class DashboardController(
 ) {
 
     @GetMapping
-    fun getAllUsers(): List<UserDto> {
-        return userService.getAllUsers()
+    fun searchUsers(
+        @RequestParam(required = false, defaultValue = "") search: String,
+        pageable: Pageable
+    ): Page<UserDto> {
+        return userService.searchUsers(search, pageable)
     }
 }
